cc0ba0dd27ce386b3283d3d1b799d300cf0b759c
Samuel Jansem <samuel.jansem@gmail.com>
2022-03-28 21:57:36 -0300

    indentation

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,19 @@ fn main() {
     println!("{}", secret_number);
 
     loop {
-        let mut guess = String::new();
         println!("Please input your guess:");
+
+        let mut guess = String::new();
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
         println!("You guessed {}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Smaller!"),
